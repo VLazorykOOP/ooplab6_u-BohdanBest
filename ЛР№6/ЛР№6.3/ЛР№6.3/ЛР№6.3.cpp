5a5ebc8b2438dfce578b99ae043b9f1a2edcd338
BohdanBest <107474110+BohdanBest@users.noreply.github.com>
2023-03-29 16:23:23 +0300

    Add files via upload

---
+++ ЛР№6/ЛР№6.3/ЛР№6.3/ЛР№6.3.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+protected:
+    string name;
+    int age;
+public:
+    Student(const string& name, int age) : name(name), age(age) {}
+    virtual ~Student() {}
+
+    friend ostream& operator<<(ostream& os, const Student& student) {
+        os << "Student name: " << student.name << endl << "age: " << student.age << endl;
+        return os;
+    }
+};
+
+class FamilyFather {
+protected:
+    string familyName;
+    int childrenCount;
+    int age2;
+public:
+    FamilyFather(const string& familyName, int childrenCount, int age2) : familyName(familyName), childrenCount(childrenCount), age2(age2){}
+    virtual ~FamilyFather() {}
+
+    friend ostream& operator<<(ostream& os, const FamilyFather& father) {
+        os << "Family father name: " << father.familyName << endl << "age: " << father.age2 << endl << "children count: " << father.childrenCount << endl;
+        return os;
+    }
+};
+
+class StudentFamilyFather : public Student, public FamilyFather {
+public:
+    StudentFamilyFather(const string& name, int age, const string& familyName, int childrenCount, int age2)
+        : Student(name, age), FamilyFather(familyName, childrenCount, age2) {}
+
+    ~StudentFamilyFather() {}
+
+    friend ostream& operator<<(ostream& os, const StudentFamilyFather& studentFather) {
+        os << static_cast<const Student&>(studentFather) << static_cast<const FamilyFather&>(studentFather);
+        return os;
+    }
+};
+
+int main() {
+    StudentFamilyFather sf("Bohdan", 18, "Bestanchuk", 2, 50);
+    cout << sf << endl;
+
+    return 0;
+}
---
+++ ЛР№6/Лр№6.2/Лр№6.2/Лр№6.2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+// Без віртуального успадкування
+class Base {
+public:
+    int base_data;
+};
+
+class Derived1 : public Base {
+public:
+    int derived1_data;
+};
+
+class Derived2 : public Base {
+public:
+    int derived2_data;
+};
+
+class Derived3 : public Derived1 {
+public:
+    int derived3_data;
+};
+
+class Derived4 : public Derived2 {
+public:
+    int derived4_data;
+};
+
+class Derived5 : public Derived3, public Derived4, public Base {
+public:
+    int derived5_data;
+};
+
+// З віртуальним успадкуванням
+class VBase {
+public:
+    int vbase_data;
+};
+
+class VDerived1 : virtual public VBase {
+public:
+    int vderived1_data;
+};
+
+class VDerived2 : virtual public VBase {
+public:
+    int vderived2_data;
+};
+
+class VDerived3 : virtual public VDerived1 {
+public:
+    int vderived3_data;
+};
+
+class VDerived4 : virtual public VDerived2 {
+public:
+    int vderived4_data;
+};
+
+class VDerived5 : virtual public VDerived3, virtual public VDerived4, virtual public VBase {
+public:
+    int vderived5_data;
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    Derived1 d1;
+    Derived2 d2;
+    Derived3 d3;
+    Derived4 d4;
+    Derived5 d5;
+    VDerived1 vd1;
+    VDerived2 vd2;
+    VDerived3 vd3;
+    VDerived4 vd4;
+    VDerived5 vd5;
+   
+    cout << "Розмір об'єкта Derived1 (без віртуального успадкування): " << sizeof(d1) << endl;
+    cout << "Розмір об'єкта Derived2 (без віртуального успадкування): " << sizeof(d2) << endl;
+    cout << "Розмір об'єкта Derived3 (без віртуального успадкування): " << sizeof(d3) << endl;
+    cout << "Розмір об'єкта Derived4 (без віртуального успадкування): " << sizeof(d4) << endl;
+    cout << "Розмір об'єкта Derived5 (без віртуального успадкування): " << sizeof(d5) << endl;
+    cout << "Розмір об'єкта VDerived1 (з віртуальним успадкуванням): " << sizeof(vd1) << endl;
+    cout << "Розмір об'єкта VDerived2 (з віртуальним успадкуванням): " << sizeof(vd2) << endl;
+    cout << "Розмір об'єкта VDerived3 (з віртуальним успадкуванням): " << sizeof(vd3) << endl;
+    cout << "Розмір об'єкта VDerived4 (з віртуальним успадкуванням): " << sizeof(vd4) << endl;
+    cout << "Розмір об'єкта VDerived5 (з віртуальним успадкуванням): " << sizeof(vd5) << endl;
+
+    return 0;
+}
